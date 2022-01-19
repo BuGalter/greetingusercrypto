37f27a2471f263aee797728ea14168982f5cab62
BuGalter <wardoc78@gmail.com>
2022-01-19 12:15:58 +0300

    Change test data

--- src/server/api/v3/user.ts
+++ src/server/api/v3/user.ts
@@ -1,13 +1,13 @@
-import User from '../../interfaces/User';
-import Session from '../../interfaces/Session';
-import { output, getUUID, } from '../../utils';
+import { createUser, } from '../../models/v3User';
+import { createSession, } from '../../models/v3Session';
+import { output, } from '../../utils';
 import { users, } from '../../../storage/users-data';
 
 export async function greetingUser(r) {
   const len: number = users.length;
   for (let i = 0; i < len; i += 1) {
     if (users[i].id === r.params.id) {
-      return output({ message: `Hi, ${ users[i].name } ${ users[i].surname }!`, });
+      return output({ message: `Hi, ${ users[i].username }!`, });
     }
   };
   return output({ message: 'User undifened', });
--- src/server/models/v3Session.ts
+++ src/server/models/v3Session.ts
@@ -1,7 +1,7 @@
 import Session from '../interfaces/Session';
 import { getUUID, } from '../utils';
 
-export function createUser(userId: string): Session {
+export function createSession(userId: string): Session {
   return {
     id: getUUID(),
     userId,
