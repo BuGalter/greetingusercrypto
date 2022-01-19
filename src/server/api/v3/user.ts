fd6c63d350859b3fae6550ff3479e95019048667
BuGalter <wardoc78@gmail.com>
2022-01-19 17:32:51 +0300

    fix: Delete some code
    Removed extra code, made auxiliary functions asynchronous

--- src/server/api/v3/user.ts
+++ src/server/api/v3/user.ts
@@ -8,7 +8,7 @@ import * as jwt from 'jsonwebtoken';
 const tokenKeyAccess = '1a2b-3c4d-5e6f-7g8h';
 const tokenKeyRefresh = '1a2b-3c4d-34tr-7g8h';
 
-function checkParams(payload) {
+async function checkParams(payload) {
   if (!payload.username || !payload.password) {
     return false;
   }
@@ -16,7 +16,7 @@ function checkParams(payload) {
   return true;
 }
 
-function findUser(username, password) {
+async function findUser(username, password) {
   for (let i = 0; i < users.length; i += 1) {
     if (users[i].username === username && users[i].password === password) {
       return users[i];
@@ -24,13 +24,13 @@ function findUser(username, password) {
   }
 }
 
-function createTokensJWT(sessionId, userId) {
+async function createTokensJWT(sessionId, userId) {
   const access = jwt.sign({ sessionId, userId, }, tokenKeyAccess, { expiresIn: 60 * 60, });
   const refresh = jwt.sign({ sessionId, userId, }, tokenKeyRefresh, { expiresIn: '10h', });
   return { access, refresh, };
 }
 
-function decodeTokenJwt(token: string) {
+async function decodeTokenJwt(token: string) {
   try {
     return jwt.verify(token, tokenKeyAccess);
   } catch (e) {
@@ -38,8 +38,8 @@ function decodeTokenJwt(token: string) {
   }
 }
 
-function validateSession(token: string) {
-  const data = decodeTokenJwt(token);
+async function validateSession(token: string) {
+  const data = await decodeTokenJwt(token);
   for (let i = 0; i < sessions.length; i += 1) {
     if (data.sessionId === sessions[i].id && data.userId === sessions[i].userId) {
       return { isValide: true, userId: sessions[i].userId, };
@@ -50,13 +50,12 @@ function validateSession(token: string) {
 }
 
 export async function greetingUser(r) {
-  console.log(r.headers);
   const token = r.headers.authorization;
   if (!token) {
     return output({ message: 'Access denied!', });
   }
 
-  const valide = validateSession(token);
+  const valide = await validateSession(token);
   if (valide.isValide) {
     for (let i = 0; i < users.length; i += 1) {
       if (users[i].id === valide.userId) {
@@ -69,30 +68,25 @@ export async function greetingUser(r) {
 }
 
 export async function userRegistration(r) {
-  const payload = r.payload;
-  if (checkParams(payload)) {
-    if (findUser(payload.username, payload.password)) {
-      console.log(users);
-      return output({ message: `Hi, ${ payload.username }!`, });
+  if (await checkParams(r.payload)) {
+    if (await findUser(r.payload.username, r.payload.password)) {
+      return output({ message: `Hi, ${ r.payload.username }!`, });
     }
 
-    users.push(createUser(payload.username, payload.password));
-    console.log(users);
-    return output({ message: `${ payload.username } added!`, });
+    users.push(createUser(r.payload.username, r.payload.password));
+    return output({ message: `${ r.payload.username } added!`, });
   }
 
-  console.log(users);
   return output({ message: 'Wrong password or login!', });
 }
 
 export async function userAuth(r) {
-  const payload = r.payload;
-  if (checkParams(payload)) {
-    const user = findUser(payload.username, payload.password);
+  if (await checkParams(r.payload)) {
+    const user = await findUser(r.payload.username, r.payload.password);
     if (user !== undefined) {
       const session = createSession(user.id);
       sessions.push(session);
-      const tokens = createTokensJWT(session.id, session.userId);
+      const tokens = await createTokensJWT(session.id, session.userId);
       return output({message: `Access Token: ${tokens.access} Refresh Token: ${tokens.refresh}`, });
     }
 
