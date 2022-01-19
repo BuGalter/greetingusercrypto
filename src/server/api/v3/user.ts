809d3e3307f428944b4ddba6f5468b029845e65f
BuGalter <wardoc78@gmail.com>
2022-01-19 13:34:06 +0300

    feat: Added addition user
    Added function to check availability username and password in request
    Added function to create new user

--- src/server/api/v3/user.ts
+++ src/server/api/v3/user.ts
@@ -3,12 +3,39 @@ import { createSession, } from '../../models/v3Session';
 import { output, } from '../../utils';
 import { users, } from '../../../storage/users-data';
 
+function checkParams(reqPayload) {
+  if (!reqPayload.username || !reqPayload.password) {
+    return false;
+  }
+
+  return true;
+}
+
 export async function greetingUser(r) {
   const len: number = users.length;
   for (let i = 0; i < len; i += 1) {
     if (users[i].id === r.params.id) {
       return output({ message: `Hi, ${ users[i].username }!`, });
     }
-  };
+  }
+
   return output({ message: 'User undifened', });
+}
+
+export async function userRegistration(r) {
+  const reqPayload = r.payload;
+  if (checkParams(reqPayload)) {
+    for (let index = 0; index < users.length; index += 1) {
+      if (users[index].username === reqPayload.username
+        && users[index].password === reqPayload.password) {
+        return output({ message: `Hi, ${ users[index].username }!`, });
+      }
+    }
+
+    users.push(createUser(reqPayload.username, reqPayload.password));
+    return output({ message: 'User added!', });
+  }
+
+  console.log(users);
+  return output({message: 'Wrong username or password!'});
 }
--- src/server/routes/v3/user.ts
+++ src/server/routes/v3/user.ts
@@ -1,6 +1,11 @@
-import { greetingUser, } from '../../api/v3/user';
+import { greetingUser, userRegistration, } from '../../api/v3/user';
 
 export default [
+  {
+    method: 'POST',
+    path: '/v3/user/reg',
+    handler: userRegistration,
+  },
   {
     method: 'GET',
     path: '/v3/user/{id}',
