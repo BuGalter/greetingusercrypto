ab2c40559ba7440a2d23948974ac9b77e1fa85f2
BuGalter <wardoc78@gmail.com>
2022-01-20 17:21:55 +0300

    feat: Added to functions in utils/token.ts descriptions
    fix: Change returns values in function decodeTokenJwt

--- src/server/utils/v3/token.ts
+++ src/server/utils/v3/token.ts
@@ -2,6 +2,12 @@ import * as jwt from 'jsonwebtoken';
 import { configSafety, } from '../../config/v3/config';
 
 export async function createTokensJWT(sessionId: string, userId: string) {
+  /**
+   * The library is used to get tokens jsonwebtoken. Default encryption is SHA256.
+   * @param {string} sessionId - User session id.
+   * @param (string) userId - User id.
+   * @returns {Object<string>} access - User access token. refresh - User refresh token. 
+   */
   const access = jwt.sign({ sessionId, userId, }, configSafety.tokenKeyAccess,
     { expiresIn: configSafety.lifeTimeAccessToken, });
 
@@ -12,10 +18,16 @@ export async function createTokensJWT(sessionId: string, userId: string) {
 }
 
 export async function decodeTokenJwt(token: string) {
+  /**
+   * The library is used to verify tokens jsonwebtoken.
+   * @param (string) token - Token which user came.
+   * @returns {object or boolean} result - Decoded data or false.
+   */
   try {
-    return jwt.verify(token, configSafety.tokenKeyAccess);
+    const result = jwt.verify(token, configSafety.tokenKeyAccess);
+    return result;
   }
   catch (e) {
-    console.log(e);
+    return false;
   }
 }
