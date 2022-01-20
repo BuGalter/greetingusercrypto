60250b160d812d436e6c1e80948d40213237106a
BuGalter <wardoc78@gmail.com>
2022-01-20 16:23:29 +0300

    feat: Added config for db and safety

---
+++ src/server/config/v3/config.ts
@@ -0,0 +1,13 @@
+export const configSafety = {
+  tokenKeyAccess: '1a2b-3c4d-5e6f-7g8h',
+  tokenKeyRefresh: '1a2b-3c4d-34tr-7g8h',
+  lifeTimeAccessToken: 1200,
+  lifeTimeRefreshToken: '10h',
+};
+
+export const configDb = {
+  database: 'cryptodb',
+  username: 'cryptodb',
+  password: '123',
+  host: 'localhost',
+};
