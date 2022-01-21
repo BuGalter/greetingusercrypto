e6f6c200a8439be5b637b645f363fd04b7035a2d
BuGalter <wardoc78@gmail.com>
2022-01-21 16:02:49 +0300

    feat: Added descriptions to functions in utils/db

--- src/server/utils/v3/db.ts
+++ src/server/utils/v3/db.ts
@@ -2,6 +2,12 @@ import { User, } from '../../models/v3/User';
 import { Session, } from '../../models/v3/Session';
 
 export async function addDbUser(userName: string, password: string): Promise<boolean> {
+  /**
+   * Function to add a user.
+   * @param {string} userName - User name.
+   * @param (string) password - User password.
+   * @returns {boolean} Has a user been added.
+   */
   try {
     const newUser = await User.create({ userName, password, });
     console.log(newUser);
@@ -14,6 +20,11 @@ export async function addDbUser(userName: string, password: string): Promise<boo
 }
 
 export async function addDbSession(userId: string) {
+  /**
+   * Function to add a session.
+   * @param {string} userId - User id.
+   * @returns {boolean} Has a user been added or {string} User id.
+   */
   try {
     const newSession = await Session.create({ userId, });
     console.log(newSession);
@@ -26,6 +37,12 @@ export async function addDbSession(userId: string) {
 }
 
 export async function findDbUser(userName: string, password: string) {
+  /**
+   * Function to find user.
+   * @param {string} userName - User name.
+   * @param {string} password - User password.
+   * @returns {boolean} If the user is not found or {string} User id.
+   */
   const user = await User.findOne({ where: { userName, password, }, });
   if (!user) {
     return false;
@@ -35,6 +52,11 @@ export async function findDbUser(userName: string, password: string) {
 }
 
 export async function findDbUseById(userId: string) {
+  /**
+   * Function to find user by id.
+   * @param {string} userId - User id.
+   * @returns {boolean} If the user is not found or {string} User name.
+   */
   const user = await User.findByPk(userId);
   if (!user) {
     return false;
